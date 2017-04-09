caaf4ff24025f9d8e5a2442e5d32b7b42241369c
anchit1501 <1501.anchit@gmail.com>
2017-04-09 11:03:28 +0530

    Initial Commit

---
+++ qkComm.py
@@ -0,0 +1,134 @@
+import copy
+import qkCommChannel
+
+
+class qkComm(object):
+
+    PHOTON_PULSE_SIZE = 128
+    MIN_SHARED_REQ = 25
+
+    def __init__(self):
+        self.random_basis = []
+        self.other_basis = []
+        self.photon_polars = []
+        self.shared_key = []
+        self.sub_shared_key = []
+        self.decision = -1
+        self.other_decision = -1
+        self.valid_key = -1
+
+#BASIS STUFF
+    #Retrieve sender's basis and remove from channel
+    def get_other_basis(self, insecure_comm_channel):
+        assert isinstance(insecure_comm_channel, qkCommChannel.qkCommChannel), 'Invalid Arg'
+        #Retrieve & Remove sender's basis from communication channel
+        if self.PHOTON_PULSE_SIZE == len(insecure_comm_channel.basis_check):
+            self.other_basis = copy.deepcopy(insecure_comm_channel.basis_check)
+            insecure_comm_channel.basis_check = []
+
+    #Compare sent and received basis
+    def compare_basis(self):
+        if len(self.other_basis) == self.PHOTON_PULSE_SIZE and len(self.random_basis) == self.PHOTON_PULSE_SIZE:
+            i = 0
+            count = 0
+            while i < self.PHOTON_PULSE_SIZE:
+                if self.random_basis[i] == self.other_basis[i]:
+                    count += 1
+                i += 1
+        if i >= self.MIN_SHARED_REQ:
+            print(count)
+        else:
+            print(count)
+
+    #send_pulse basis to a qkCommChannel for a send_pulseer/Alice
+    def send_pulse_basis(self, insecure_channel, basis_c):
+        assert isinstance(insecure_channel, qkCommChannel.qkCommChannel), 'Invalid Arg1'
+        insecure_channel.basis_check = basis_c
+
+    #Drops any polarizations where the sender and receiver basis differ'd
+    def drop_invalid_polars(self):
+        if len(self.other_basis) == self.PHOTON_PULSE_SIZE and len(self.random_basis) == self.PHOTON_PULSE_SIZE:
+            i = 0
+            max_size = self.PHOTON_PULSE_SIZE
+            while i < max_size:
+                if self.random_basis[i] != self.other_basis[i]:
+                    self.random_basis.pop(i)
+                    self.other_basis.pop(i)
+                    self.photon_polars.pop(i)
+                    max_size -= 1
+                else:
+                    i += 1
+        else:
+            print("Error - Required Data Not Initialized.")
+
+#BIT AND SUBBIT STRING STUFF
+    #Analyzes the recorded polarizations to determine the shared key received from a sender through a channel.
+    def set_bit_string(self):
+        self.shared_key = []
+        if len(self.photon_polars) <= self.PHOTON_PULSE_SIZE and len(self.photon_polars) > 0:
+            i = 0
+            while i < len(self.photon_polars):
+                if self.photon_polars[i] == 0:
+                    self.shared_key.append(0)
+                elif self.photon_polars[i] == 90:
+                    self.shared_key.append(1)
+                elif self.photon_polars[i] == 45:
+                    self.shared_key.append(0)
+                elif self.photon_polars[i] == 135:
+                    self.shared_key.append(1)
+                i += 1
+
+    #Retrieve a subBitString from a qkCommChannel
+    def get_sub_bit_string(self, insecure_channel):
+        self.sub_shared_key = []
+        assert isinstance(insecure_channel, qkCommChannel.qkCommChannel), 'Invalid Arg1'
+        if len(insecure_channel.sub_shared_key) > 0:
+            self.sub_shared_key = copy.deepcopy(insecure_channel.sub_shared_key)
+            insecure_channel.sub_shared_key = []
+        else:
+            print("Error - qkCommChannel has no bit sub string.")
+
+    #send_pulses a subBitString to a qkCommChannel, the sub key is just shared_key[0->approx half]
+    def send_pulse_sub_bit_string(self, insecure_comm_channel):
+        assert isinstance(insecure_comm_channel, qkCommChannel.qkCommChannel), 'Invalid Arg1'
+        insecure_comm_channel.sub_shared_key = []
+        if len(self.shared_key) > 0:
+            insecure_comm_channel.sub_shared_key = copy.deepcopy((self.shared_key[0: (int((len(self.shared_key))/2))]))
+
+#DECISION STUFF
+    #Sender sends a decision to a qkCommChannel
+    def send_pulse_decision(self, insecure_comm_channel):
+        assert isinstance(insecure_comm_channel, qkCommChannel.qkCommChannel), 'Invalid Arg1'
+        if self.decision != -1:
+            insecure_comm_channel.decision = self.decision
+        else:
+            print("Error - send_pulse_decision() - decision - Invalid")
+
+    #Retrieves a decision value from a qkCommChannel
+    def get_decision(self, insecure_comm_channel):
+        assert isinstance(insecure_comm_channel, qkCommChannel.qkCommChannel), 'Invalid Arg1'
+        if insecure_comm_channel != -1:
+            self.other_decision = insecure_comm_channel.decision
+            if self.decision == 1 and self.other_decision == 1:
+                self.valid_key = 1
+            else:
+                self.valid_key = 0
+        else:
+            print("Error - send_pulse_decision() - decision - Invalid")
+
+    #Decides if a sub shared key is valid enough, to validate a shared key
+    def decide(self):
+        if len(self.sub_shared_key) > 0 and len(self.shared_key) > len(self.sub_shared_key):
+            i = 0
+            count = 0
+            while i < len(self.sub_shared_key):
+                if self.sub_shared_key[i] == self.shared_key[i]:
+                    count += 1
+                i += 1
+            if count >= self.MIN_SHARED_REQ:
+                self.decision = 1
+            else:
+                self.decision = 0
+        else:
+            self.decision = 0
+            print("Error - decide() - sub_shared_key || shared_key - Invalid")
---
+++ qkTester.py
@@ -0,0 +1,70 @@
+import qkSender
+import qkCommChannel
+import qkReceiver
+import copy
+
+    # Entity Creation
+
+Alice = qkSender.qkSender()
+insecure_channel = qkCommChannel.qkCommChannel()
+Bob = qkReceiver.qkReceiver()
+#Eve = qkAttacker.qkAttacker()
+
+    # Alice creates a randomized pulse composed of photons, she also records the basis for each photon.
+    # Alice then sends the photon pulse.
+    # Bob then attempts to measure the photon pulse, by randomly generating his own basis for each photon in the pulse.
+
+Alice.create_photon_pulse()
+Alice.send_pulse(insecure_channel, copy.deepcopy(Alice.photon_pulse))
+#Eve.attack(insecure_channel)
+Bob.measure_polarizations(insecure_channel)
+
+    #Alice sends basis to Bob
+
+Alice.send_pulse_basis(insecure_channel, copy.deepcopy(Alice.random_basis))
+Bob.get_other_basis(insecure_channel)
+
+    #Bob sends basis to Alice
+
+Bob.send_pulse_basis(insecure_channel, copy.deepcopy(Bob.random_basis))
+Alice.get_other_basis(insecure_channel)
+
+    #Alice and bob compare basis to see if a certain # of shared bits was reached.
+
+Alice.drop_invalid_polars()
+Bob.drop_invalid_polars()
+
+    #Alice and Bob both break their polarizations down into bit strings/shared_key
+
+Alice.set_bit_string()
+Bob.set_bit_string()
+
+    #Alice and Bob compare sub set of the bit string (photon polarizations -> bits)
+
+Alice.send_pulse_sub_bit_string(insecure_channel)
+Bob.get_sub_bit_string(insecure_channel)
+Bob.send_pulse_sub_bit_string(insecure_channel)
+Alice.get_sub_bit_string(insecure_channel)
+
+    #Alice and Bob decide if each others sub shared keys are "equal" enough to trust the channel and declare a shared key
+
+Alice.decide()
+Bob.decide()
+
+    #Alice and Bob send_pulse there decisions on whether the shared key is valid, by send_pulseing the decision to each other
+
+Alice.send_pulse_decision(insecure_channel)
+Bob.get_decision(insecure_channel)
+Bob.send_pulse_decision(insecure_channel)
+Alice.get_decision(insecure_channel)
+
+    #Alice and Bob can now try again in a new channel or use the shared key based on the decision
+
+if Alice.valid_key == 1 and Bob.valid_key == 1:
+    if Alice.shared_key == Bob.shared_key:
+        print("Alice & Bob's Shared Secret Key:  ", Alice.shared_key)
+        print("Length Of Shared Secret Key:     ", len(Alice.shared_key))
+    else:
+        print("Error in implementation (if no attacker set/req # of sub shared reached)")
+else:
+    print("Alice & Bob have not agreed upon a Shared Secret Key")
